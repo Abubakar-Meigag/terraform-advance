276f91b5d64780e67afc52e810ae14863776aa5b
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-12-21 12:53:46 +0000

    update

--- manifests/main.tf
+++ manifests/main.tf
@@ -1,6 +1,6 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myEc2vm" {
-  ami = "ami-0eb26da1d86aecc88 "
+  ami = "ami-019374baf467d6601"
   instance_type = "t3.micro"
   user_data = file("${path.module}/app1-install.sh")
   tags = {
